f21f8724923ca745ed24370b6ebf9d5ff4acad91
chz <abcde228866@126.com>
2026-03-24 12:36:33 +0800

    init

---
+++ src/main/java/com/code/codewarden/CodeWardenApplication.java
@@ -0,0 +1,13 @@
+package com.code.codewarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeWardenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/codewarden/CodeWardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.codewarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
